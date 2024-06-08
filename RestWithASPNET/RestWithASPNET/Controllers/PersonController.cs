9a46865136153acf59362f0df19d3a61d5940170
CaioAval <caio@agilean.com.br>
2024-06-08 18:02:46 -0300

    Implementing a CRUD on the application

---
+++ RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Services;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<CalculatorController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult FindAll()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult FindById(long id)
+        {
+            var person = _personService.FindById(id);
+
+            if(person == null)
+                return NotFound();
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Model.Person person)
+        {
+            if (person == null)
+                return BadRequest();
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Model.Person person)
+        {
+            if (person == null)
+                return BadRequest();
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ RestWithASPNET/RestWithASPNET/Services/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person? FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+        void Delete(long Id);
+
+    }
+}
